0b1629a2f50630681039ce0d6dfdf920b45e6da4
chenyongsuda <chenyongsuda@gmail.com>
2017-07-09 14:29:31 +0800

    1.添加微信认证

--- WeChatPub.py
+++ WeChatPub.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask import request
+import hashlib
 
 app = Flask(__name__)
 
@@ -6,6 +8,26 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/weixin')
+def weixin():
+    signature = request.args.get('signature', '')
+    timestamp = request.args.get('timestamp', '')
+    nonce = request.args.get('nonce', '')
+    echostr = request.args.get('echostr', '')
+    token = "chenyongsuda"
+
+    data_list = [token, timestamp, nonce]
+    sorted(data_list)
+    sha1 = hashlib.sha1()
+    map(sha1.update, data_list)
+    hashcode = sha1.hexdigest()
+    print "handle/GET func: hashcode, signature: ", hashcode, signature
+    if hashcode == signature:
+        return echostr
+    else:
+        return ""
 
 
 if __name__ == '__main__':
